1b5b7fb9468c9eef7d35422c492391c30348161c
Kane Koo <kanekoo@gmail.com>
2019-05-12 17:22:59 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,15 +4,16 @@ def reformat_languages(languages)
   # your code here
   reform_hash={}
   hash_elem={}
-  style=[]
+  style= []
   index = 0
  # binding.pry
   languages.each do |key, keyval2| 
     
   #  binding.pry
-        puts "id #{index}, key #{key}, keyval2: #{keyval2}"
+       
         index += 1
     keyval2.each do |key2,typehash|
+       puts "id #{index}, key #{key2}, keyval2: #{typehash}"
      hash_elem = reform_hash[key2]
     # binding.pry
  
