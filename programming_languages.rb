2940412c312dfb264e034c4635cae15066e0db64
Kane Koo <kanekoo@gmail.com>
2019-05-12 14:33:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  reform_hash={}
+  languages.each do |key, keyval2| 
+    keyval2 each do |key2,type|
+      binding.pry
+      reform[key2] = {type: 
+      
 end
