3115645a0e3ac4d031d20c7c934cbe00fb227ae4
Kane Koo <kanekoo@gmail.com>
2019-05-12 15:05:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
     keyval2.each do |key2,typehash|
      hash_elem = reform_hash[key2]
     # binding.pry
-     puts "key2 #{key}, keyval2: #{typehash}"
+     puts "key2 #{key}, typehash: #{typehash}"
  
   
       typehash.each do |key3, type|
