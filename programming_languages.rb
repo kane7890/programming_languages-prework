54d341f536ab6033f3788146286008e14325da60
Kane Koo <kanekoo@gmail.com>
2019-05-12 17:56:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -32,7 +32,7 @@ def reformat_languages(languages)
 
            puts "id #{index}, key3 #{key3}, typeval: #{typeval}"
            
-        binding.pry
+ #       binding.pry
         if hash_elem[typeval].include?(key) == false
           hash_elem[typeval] << key
         end
